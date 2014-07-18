3f2e1db5f89e1ede05a84b2e150f66154c96d7c8
Drake Griffith <Drakegriffith8@gmail.com>
2014-07-18 16:33:09 -0400

    Over Vioew Of Last Time
    But Added Breack
    while true and elif

---
+++ Poop.py
@@ -0,0 +1,46 @@
+
+name = input ("What's Your Name")
+name = name.strip()
+name = name.title()
+
+
+while True:
+    print ("Hello " + name)
+
+
+    color = input ("What Ur Favorite Color")
+    color = color.strip()
+    color = color.title()
+
+     
+
+
+    print ("Hey , " + color + " Thats My Favorite Color Too!")
+     
+
+    booger = input("Do U Pick Ur Nose?")
+
+    if answer == "yes":
+        print ("Thats Gross")
+        break
+    elif answer == "no":
+        print ("Ok Thats Good")
+
+    elif answer == "mabye":
+        print ("Umm...")
+
+    else:
+        print("Ok Then !")
+
+print("Alrighty Then")
+
+     
+      
+
+
+
+
+
+
+
+
