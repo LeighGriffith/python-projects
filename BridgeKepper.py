19c167f74b8a14afa172a4865c9608fa797a2ba6
Drake Griffith <Drakegriffith8@gmail.com>
2014-08-08 17:26:45 -0400

    The Bridge Keeper
    practiced if statements and made the youtube video "the bridge keeper"
    very fun =D

---
+++ BridgeKepper.py
@@ -0,0 +1,50 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:45:13) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+Ok drakewhat is your quest? 
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+Ok drake what is your quest? poop
+What is your favorite color? red
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+Ok drake what is your quest? to seek the grail
+What is your favorite color? red
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+Ok drake what is your quest? 
+>>> ================================ RESTART ================================
+>>> 
+What is your name? rob
+Ok rob what is your quest? 
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+You answered: drake
+Ok drake what is your quest? 
+>>> ================================ RESTART ================================
+>>> 
+What is your name? drake
+You answered: drake
+Ok drake what is your quest? bob
+You answered: bob
+What is your favorite color? red
+You answered: red
+>>> ================================ RESTART ================================
+>>> 
+What is your name? 
+>>> ================================ RESTART ================================
+>>> 
+What is your name? Drake
+You answered: drake
+Ok drake what is your quest? to the sadbnjsdabjdsa
+You answered: to the sadbnjsdabjdsa
+What is your favorite color? blue
+You answered: blue
+Fine, off you go then
+>>> 
