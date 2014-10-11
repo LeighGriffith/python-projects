88db72996e9ba301cbdef892790590b8074a0bae
Drake Griffith <Drakegriffith8@gmail.com>
2014-10-11 10:30:53 -0400

    Made first drawing in python!!!
    Its blu8e and its amazing

---
+++ Chinese_quiz.py
@@ -0,0 +1,7 @@
+quiz = input("Wanna do a chinese quiz? ").strip().lower()
+
+    if 'y' in quiz:
+        print("Ok then!!")
+
+    else:
+        print("Fine then...")
---
+++ code-studio/s1level24.py
@@ -0,0 +1,18 @@
+'''
+moveForward(100);
+turnRight(90);
+moveForeword(100);
+'''
+
+
+from turtle import Turtle
+
+artist = Turtle()
+artist.pensize(7)
+artist.pencolor('blue')
+
+artist.forward(100)
+artist.right(90)
+artist.forward(100)
+
+
