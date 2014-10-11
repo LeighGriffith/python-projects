fbaa4c6c29f06a666872e10bf7e98aa61b75224a
Drake Griffith <Drakegriffith8@gmail.com>
2014-10-11 10:41:11 -0400

    Made level 25 in artist
    drawed a square

---
+++ code-studio/s1level25.py
@@ -0,0 +1,28 @@
+'''
+moveForward(100);
+turnRight(90);
+moveForward(100);
+turnRight(90);
+moveForward(100);
+turnRight(90);
+moveForward(100);
+turnRight(90);
+'''
+
+ 
+
+from turtle import Turtle
+
+artist = Turtle()
+artist.pensize(7)
+artist.pencolor('red')
+
+artist.forward(100)
+artist.right(90)
+artist.forward(100)
+artist.right(90)
+artist.forward(100)
+artist.right(90)
+artist.forward(100)
+artist.right(90)
+    
