fe443b7a0c252b6f4c8eb3af882fcd33630d8f97
Drake Griffith <Drakegriffith8@gmail.com>
2014-10-18 12:08:45 -0400

    random loops in ports
    goes in sqares

---
+++ code-studio/s1level33.py
@@ -0,0 +1,14 @@
+'''
+s1level33
+'''
+
+import turtle
+artist = turtle.Turtle()
+artist.pensize(7)
+artist.pencolor('purple')
+artist.speed(100)
+
+for count in range(100):
+    artist.forward(25)
+    artist.right(90)
+    artist.forward(10)
